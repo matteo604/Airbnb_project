9dfd4a54eae6c27a8d865308c13f7479ba37ff24
chipmstr <dan.ursu@hotmail.com>
2024-10-14 12:14:08 +0200

    Add Devise views for log in, sign up and Searchbar

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,12 @@ Rails.application.routes.draw do
   # Nested routes for properties and bookings
   resources :properties do
     resources :bookings, only: [:new, :create]  # Bookings can be created under properties
+    collection do
+      get :search  # Adding search route for properties
+    end
   end
 
-  # Nested routes for bookings and reviews
+  # nnested routes for bookings and reviews
   resources :bookings do
     resources :reviews, only: [:create, :destroy]  # Reviews can be created or deleted under bookings
   end
